8c04bbf02572839bc45c3222009947ed1c23e1ea
Dimitar Tsvetanov? <dimitarcho93@gmail.com>
2022-10-30 15:54:30 +0200

    Create top of Sidebar component

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <ColorModeContext.Provider value={colorMode}>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
-                <div className="App">
+                <div className="app">
                     <Sidebar />
                     <main className="content">
                         <Topbar />
--- src/scenes/global/Sidebar.js
+++ src/scenes/global/Sidebar.js
@@ -75,6 +75,43 @@ const Sidebar = () => {
                             </Box>
                         )}
                     </MenuItem>
+                    {/* USER */}
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box
+                                display="flex"
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={"../../assets/user.png"}
+                                    style={{
+                                        cursor: "pointer",
+                                        borderRadius: "50%",
+                                    }}
+                                />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{ m: "10px 0 0 0" }}
+                                >
+                                    Dimitar
+                                </Typography>
+                                <Typography
+                                    variant="h5"
+                                    color={colors.greenAccent[500]}
+                                >
+                                    VP Fancy Admin
+                                </Typography>
+                            </Box>
+                        </Box>
+                    )}
                 </Menu>
             </ProSidebar>
         </Box>
