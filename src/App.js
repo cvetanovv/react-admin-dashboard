8d985505abca70d601ea0f8a7d3cbe6972af9619
Dimitar Tsvetanov? <dimitarcho93@gmail.com>
2022-10-30 10:10:03 +0200

    Add collapsed menu on Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -9,8 +22,12 @@ function App() {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className="App">
+                    <Sidebar />
                     <main className="content">
                         <Topbar />
+                        <Routes>
+                            <Route path="/" element={Dashboard} />
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
