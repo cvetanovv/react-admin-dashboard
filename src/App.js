7f5d46d0d2ff10e744bd92eded8c1d9fa6613bb6
Dimitar Tsvetanov? <dimitarcho93@gmail.com>
2022-11-12 20:09:19 +0200

    Create Profile Form with validations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/Team";
 import Invoices from "./scenes/invoices/Invoices";
 import Contacts from "./scenes/contacts/Contacts";
+import Form from "./scenes/form/Form";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -23,6 +24,7 @@ function App() {
                             <Route path="/team" element={<Team />} />
                             <Route path="/contacts" element={<Contacts />} />
                             <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
                         </Routes>
                     </main>
                 </div>
