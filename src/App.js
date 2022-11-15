d4a36146b3053dca5c486c034d178cac81612a18
Dimitar Tsvetanov? <dimitarcho93@gmail.com>
2022-11-15 19:37:40 +0200

    Create Calendar component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Team from "./scenes/team/Team";
 import Invoices from "./scenes/invoices/Invoices";
 import Contacts from "./scenes/contacts/Contacts";
 import Form from "./scenes/form/Form";
+import Calendar from "./scenes/calendar/Calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -25,6 +26,7 @@ function App() {
                             <Route path="/contacts" element={<Contacts />} />
                             <Route path="/invoices" element={<Invoices />} />
                             <Route path="/form" element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
                         </Routes>
                     </main>
                 </div>
